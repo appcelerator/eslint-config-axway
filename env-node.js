1105b90ad68891dadef6d06c73c016f3a5aec891
Ewan Harris <ewanharris93@gmail.com>
2019-06-10 10:45:44 +0100

    feat: enable select rules from eslint-plugin-node

--- env-node.js
+++ env-node.js
@@ -6,5 +6,24 @@ module.exports = {
 	'parserOptions': {
 		'ecmaVersion': 9
 	},
-	'extends': './index.js'
+	'plugins': [
+		'node'
+	],
+	'extends': './index.js',
+	'rules': {
+		'node/file-extension-in-import': 'warn',
+		'node/no-deprecated-api': 'warn',
+		'node/no-unpublished-bin': 'warn',
+		'node/no-unsupported-features/es-builtins': 'warn', // TODO: Error in 5.0.0
+		'node/no-unsupported-features/es-syntax': 'warn', // TODO: Error in 5.0.0
+		'node/no-unsupported-features/node-builtins': 'warn', // TODO: Error in 5.0.0
+		'node/prefer-global/buffer': 'warn',
+		'node/prefer-global/console': 'warn',
+		'node/prefer-global/process': 'warn',
+		'node/prefer-global/text-decoder': 'warn',
+		'node/prefer-global/text-encoder': 'warn',
+		'node/prefer-global/url-search-params': 'warn',
+		'node/prefer-global/url': 'warn',
+		'node/process-exit-as-throw': 'warn'
+	}
 };
