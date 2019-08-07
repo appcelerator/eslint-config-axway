92664c0674c00a33afe6a4a6984678a04fa3e540
Ewan Harris <ewanharris93@gmail.com>
2019-08-07 10:54:47 +0100

    feat: add support for linting typescript

---
+++ +typescript.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const verifyPeer = require('./verify-peer-dependency');
+verifyPeer('@typescript-eslint/parser');
+verifyPeer('@typescript-eslint/eslint-plugin');
+
+const tsConfig = path.join(process.cwd(), 'tsconfig.json');
+
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: tsConfig
+	},
+	plugins: [ '@typescript-eslint' ],
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended'
+	]
+};
