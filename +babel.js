dac766ad41f67a5d0cd14a3ac98d6b091dacf5c8
Chris Barber <chris@cb1inc.com>
2021-02-26 17:07:29 -0600

    Added the super important Babel eslint parser.

--- +babel.js
+++ +babel.js
@@ -2,11 +2,16 @@
 /* eslint-disable quote-props */
 var verifyPeer = require('./verify-peer-dependency');
 verifyPeer('@babel/eslint-parser');
+verifyPeer('@babel/eslint-plugin');
 
 module.exports = {
 	'parser': '@babel/eslint-parser',
 	'parserOptions': {
 		'ecmaVersion': 2020,
 		'requireConfigFile': false
+	},
+	'plugins': [ '@babel' ],
+	'rules': {
+		'@babel/semi': 'off'
 	}
 };
